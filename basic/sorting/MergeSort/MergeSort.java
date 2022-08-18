a32992a000269b2a852886d6561ff4a35cc47c45
theodore <616364596@qq.com>
2022-08-18 14:42:42 +0800

    归并排序

--- basic/sorting/MergeSort/MergeSort.java
+++ basic/sorting/MergeSort/MergeSort.java
@@ -4,23 +4,38 @@ import java.util.Arrays;
 
 public class MergeSort {
 
-    public static void MergeSort(int[] nums) {
-        //自顶向下归并
+    private static int[] tmp = new int[100010];
 
-        //自底往下归并
+    public static void MergeSort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = (left + right) >> 1;
+        MergeSort(nums, left, mid);
+        MergeSort(nums, mid + 1, right);
+        int i = left, j = mid + 1, k = 0;
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                tmp[k++] = nums[i++];
+            } else {
+                tmp[k++] = nums[j++];
+            }
 
-
-    }
-
-    private static void swap(int[] nums,int i,int j){
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+        }
+        while (i <= mid) {
+            tmp[k++] = nums[i++];
+        }
+        while (j <= right) {
+            tmp[k++] = nums[j++];
+        }
+        for (i = left, j = 0; i <= right; ++i, ++j) {
+            nums[i] = tmp[j];
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,7,9,5,8};
-        MergeSort(nums);
+        int[] nums = {1, 2, 7, 9, 5, 8};
+        MergeSort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
     }
 }
